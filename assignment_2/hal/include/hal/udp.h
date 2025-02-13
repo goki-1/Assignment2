d8121ffd069a0aafd3ba4650e14576c5081e8f51
Sukhman-pb <sukhi_18@icloud.com>
2025-02-12 18:56:51 -0800

    Add files via upload

--- assignment_2/hal/include/hal/udp.h
+++ assignment_2/hal/include/hal/udp.h
@@ -1,19 +1,32 @@
 #ifndef UDP_H
 #define UDP_H
 
+#define RECVBUFLEN 64     //we will only take smaller messages
+#define SENDBUFLEN 1024   //we need to send alot of data so we need a bigger buffer
+
+//structs to pass data to thread functions i.e recv and send
+typedef struct{
+    char* buff;
+    int buff_len;
+}thread_data;
+
 // create a socket
-int createsocket();
+//return 1 on success
+//0 on failure
+int init_socket();
 
+// get socket descriptor
+//returns the socket descriptor just a helper function
+//returns -1 on failure
+int get_socket_descriptor();
 
-// send mesaage
-int sendMessage(char* message);
+//recieves the message from the socket
+void *recv_message(thread_data *recv_data);    
 
-
-// recieve message
-void* receiveMessage(void *arg);
-
+// send message to the socket
+void *send_message(thread_data *send_data);
 
 // close socket
-int closeSocket();
+int close_socket();
  
 #endif
