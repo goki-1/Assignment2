f959c2bc7d993a8570677ae97462dd258a06f292
Sukhman-pb <sukhi_18@icloud.com>
2025-02-12 18:57:40 -0800

    Add files via upload

--- assignment_2/hal/src/udp.c
+++ assignment_2/hal/src/udp.c
@@ -12,57 +12,103 @@
 
 #define SUCCESS 0
 #define FAIL 1
-#define MAXBUFLEN 1024
 #define PORT 12345
 
-struct sockaddr_in sin;
-memset(&sin, 0, sizeof(sin));
-sin.sin_family = AF_INET;
-sin.sin_addr.s_addr = htonl(INADDR_ANY);
-sin.sin_port = htons(PORT);
+//global variables
+static struct sockaddr_in sin;
+static int socketDescriptor = -1;
+static struct sockaddr_in sinRemote;
+static unsigned int sin_len = sizeof(sinRemote);
+static char message_rc[RECVBUFLEN];
+static char message_sn[SENDBUFLEN];
 
-// Function to create a UDP socket
-int createsocket(char* hostname, char* remoteport, char* myport) 
+
+// Function to create and bind  UDP socket
+int init_socket() 
 {
-    int socketDescriptor = socket(AF_INET, SOCK_DGRAM, 0);
+    socketDescriptor = socket(AF_INET, SOCK_DGRAM, 0);
+    if (socketDescriptor == -1) {
+        perror("socket failure \n");
+        return FAIL;
+    }
+    memset(&sin, 0, sizeof(sin));
+    sin.sin_family = AF_INET;
+    sin.sin_addr.s_addr = htonl(INADDR_ANY);
+    //this is where we put the port for binding the socket
+    sin.sin_port = htons(PORT);
 
-    // Resolve remote address
-   
+    if (bind(socketDescriptor, (struct sockaddr *)&sin, sizeof(sin)) < 0)
+    {
+        perror("bind failure \n");
+        return FAIL;
+    }
+    return SUCCESS;  
 }
 
-// Function to bind the socket to a local port
-int bindsocket()
+int get_socket_descriptor()
 {
-    if (sockfd == -1) {
-        fprintf(stderr, "bindsocket: Socket not initialized.\n");
-        return FAIL;
-    }
+    return socketDescriptor;
+}
 
-    if (bind(sockfd, recvres->ai_addr, recvres->ai_addrlen) == -1) {
-        perror("bindsocket: bind failed");
-        return FAIL;
+// Function to receive message from the socket
+//it will not run as an infinite loop we need to call it again and again
+void* recv_message(thread_data *recv_data)
+{
+    //copying the buffer and buffer length to local variables
+    char* buffer = recv_data->buff;
+    int buffer_length = recv_data->buff_len;
+    if(socketDescriptor == -1)
+    {
+        perror("socket descriptor is not initialized \n");
+        return NULL;
     }
+    //we will keep on recieving the message until we get a message
+    int bytes_received = recvfrom(socketDescriptor, message_rc, RECVBUFLEN - 1, 0, (struct sockaddr *)&sinRemote, &sin_len);
+    
+    if(bytes_received == -1){
+        perror("recieve error \n");
+        return NULL;
+    }
+    
+    message_rc[bytes_received] = 0; //null terminating the buffers
+    if(buffer_length < bytes_received)
+    {
+        perror("buffer length is smaller than the message length \n");
+        return NULL;
+    }
+    //if no error then copy to the buffer passed by the user 
+    snprintf(buffer, buffer_length, "%s", message_rc);
+    return NULL;
+}
 
-    return SUCCESS;
+// Function to send message to the socket
+//buffer is the message to be sent
+//buffer_length is the length of the message
+void* send_message(thread_data *send_data)
+{
+    char* buffer = send_data->buff;
+    int buffer_length = send_data->buff_len;
+    if(buffer_length > SENDBUFLEN)
+    {
+        perror("buffer length is greater than the message length \n");
+        return NULL;
+    }
+    //we will recieve the message from the buffer and we will send it via the socket
+    int bytes_sent = sendto(socketDescriptor, buffer, buffer_length, 0, (struct sockaddr *)&sinRemote, sin_len);
+    if (bytes_sent == -1) {
+        perror("sending error \n");
+        return NULL;
+    }
+    return NULL;
 }
 
 // Function to close the socket and free resources
-int closeSocket()
+int close_socket()
 {
-    if (sockfd != -1) {
-        close(sockfd);
-        sockfd = -1;
-    }
-    
-    if (res) {
-        freeaddrinfo(res);
-        res = NULL;
-    }
-
-    if (recvres) {
-        freeaddrinfo(recvres);
-        recvres = NULL;
-    }
-
-    return SUCCESS;
+   if(close(socketDescriptor))
+   {
+        return SUCCESS;
+   }
+    return FAIL;
+   
 }
