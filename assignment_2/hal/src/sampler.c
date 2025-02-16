8edb167f0340710e358a665b3e88dc467b24be56
Gurkirat Singh <101526938+goki-1@users.noreply.github.com>
2025-02-15 21:15:21 -0800

    Update sampler.c

--- assignment_2/hal/src/sampler.c
+++ assignment_2/hal/src/sampler.c
@@ -58,21 +58,31 @@ void Sampler_init(void){
 void Sampler_cleanup(void){
     isRunning = false;
     pthread_join(samplerThread, NULL);
+    close_i2c_bus(i2c_file_desc);
 }
 // Must be called once every 1s.
 // Moves the samples that it has been collecting this second into
 // the history, which makes the samples available for reads (below).
 void Sampler_moveCurrentDataToHistory(void);
+
+
 // Get the number of samples collected during the previous complete second.
-int Sampler_getHistorySize(void);
+int Sampler_getHistorySize(void) {
+    return historySampleCount;
+}
 // Get a copy of the samples in the sample history.
 // Returns a newly allocated array and sets `size` to be the
 // number of elements in the returned array (output-only parameter).
 // The calling code must call free() on the returned pointer.
 // Note: It provides both data and size to ensure consistency.
 double* Sampler_getHistory(int *size);
-// Get the average light level (not tied to the history).
-double Sampler_getAverageReading(void);
-// Get the total number of light level samples taken so far.
-long long Sampler_getNumSamplesTaken(void);
 
+
+// Get the average light level (not tied to the history).
+double Sampler_getAverageReading(void) {
+    return currentAverage;
+}
+// Get the total number of light level samples taken so far.
+long long Sampler_getNumSamplesTaken(void) {
+    return totalSamples;
+}
