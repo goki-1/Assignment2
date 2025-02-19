c682718df705f55688ea19ef47f3fb3365001676
Gurkirat Singh <101526938+goki-1@users.noreply.github.com>
2025-02-18 18:42:21 -0800

    Update pwm.h

--- assignment_2/hal/include/hal/pwm.h
+++ assignment_2/hal/include/hal/pwm.h
@@ -6,9 +6,10 @@
 #include <string.h>
 
 #define PWM_PATH "/dev/hat/pwm/GPIO12/"
-
+void int_pwn();
 void pwm_set_duty_cycle(int duty_cycle);
 void pwm_set_period(int period);
 void pwm_enable(int enable);
+
 
 #endif
