ffdf21b241635847c0e9ecc99f6113a9eb5a88ba
Sukhman-pb <sukhi_18@icloud.com>
2025-02-26 21:56:03 -0800

    Add files via upload

--- assignment_2/hal/src/pwm.c
+++ assignment_2/hal/src/pwm.c
@@ -35,8 +35,10 @@ void pwm_enable(int enable) {
 }
 
 void change_frequency(int frequency){
-
-    int period_ns = 1000000000 / frequency;
+    int period_ns = 0;
+    if(frequency >3){
+        period_ns = 1000000000 / frequency;
+    }
     pwm_set_period(period_ns);
     pwm_set_duty_cycle(period_ns / 2);
 }
