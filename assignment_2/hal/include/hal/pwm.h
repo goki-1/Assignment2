94523556e13897d36164ab2414134a55c58b7ab3
Gurkirat Singh <101526938+goki-1@users.noreply.github.com>
2025-02-27 18:15:05 -0800

    Update pwm.h

--- assignment_2/hal/include/hal/pwm.h
+++ assignment_2/hal/include/hal/pwm.h
@@ -1,15 +1,25 @@
+//this file provides a hal for PWM module by seperating out changing frequencies of the emitter
 #ifndef PWM_H
 #define PWM_H
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+//path of the file that we need to change
 #define PWM_PATH "/dev/hat/pwm/GPIO12/"
+
+//init function to initialize the values
 void init_pwm(void);
+
+//function to change the duty_cycles
 void pwm_set_duty_cycle(int duty_cycle);
+
+//function to change the period
 void pwm_set_period(int period);
+//function to enable and disable the emitter
 void pwm_enable(int enable);
+//function to change the frequency with the provided value
 void change_frequency(int frequency);
+//closing fucniton
 void close_pwm(void);
 #endif
