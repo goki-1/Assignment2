0f8bb82dd2d08a9da594ba9c9157e34c30a0e3cf
Gurkirat Singh <101526938+goki-1@users.noreply.github.com>
2025-02-27 18:21:03 -0800

    Update draw_stuff.h

--- assignment_2/app/include/draw_stuff.h
+++ assignment_2/app/include/draw_stuff.h
@@ -1,9 +1,14 @@
+//Taken from Draw Stuff files provided by instructor
 #ifndef _DRAW_STUFF_H_
 #define _DRAW_STUFF_H_
 
+//initialize the lcd
 void DrawStuff_init();
+
+//run the clean up code for lcd, turn it black
 void DrawStuff_cleanup();
 
+//runs the updates on the screen as required by program
 void DrawStuff_updateScreen(char* message);
 
 #endif
